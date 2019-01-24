40143bf4cacae767a5e9d75ac835692f5c8947b7
Felix Wu <felixxwu@gmail.com>
2019-01-24 15:50:03 +0000

    added __init__.py and updated test legal move

--- classes/testLegalMove.py
+++ classes/testLegalMove.py
@@ -1,13 +1,11 @@
 import unittest
-from move import *
+from move import Move
 
 class UnitTests(unittest.TestCase):
 
-    def test(self):
+    def testLegalMove(self):
         move = Move("K", "A1", "A2")
         self.assertEqual(move.isLegal(), None)
-        self.assertEqual(move.piece, "K")
-        self.assertEqual(move.moveTo, "A2")
 
 if __name__ == '__main__':
     unittest.main(exit=False)
