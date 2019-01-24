8b11215aac337334bd6e77852a4bb7060c68008c
Felix Wu <felixxwu@gmail.com>
2019-01-24 17:23:13 +0000

    moved unit tests into one main file

--- unitTests.py
+++ unitTests.py
@@ -1,11 +1,13 @@
 import unittest
-from move import Move
+
+from classes.move import Move
 
 class UnitTests(unittest.TestCase):
 
-    def testLegalMove(self):
+    def test(self):
         move = Move("K", "A1", "A2")
         self.assertEqual(move.isLegal(), None)
 
+# required setup for testing
 if __name__ == '__main__':
     unittest.main(exit=False)
