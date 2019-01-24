2a21219d1b204889e9f7f747ad7bd700d4c5b7c0
Felix Wu <felixxwu@gmail.com>
2019-01-24 17:23:43 +0000

    added util file

---
+++ util/util.py
@@ -0,0 +1,29 @@
+import json
+
+# maps a square string to [x, y] coordinates
+def squareToCoordinates(square):
+
+    if (len(square) != 2):
+        raise ValueError("argument 'square' must be two characters long")
+
+    # extract letter from square
+    letter = square[:1]
+
+    if (not letter.isupper() and not letter.islower()):
+        raise ValueError("argument 'square' must start with a letter")
+
+    if (letter.isupper()):
+        letter = chr(ord(letter) + 32)
+
+    x = ord(letter) - 97
+
+    if (x >= 8 or x < 0):
+        raise ValueError("argument 'square' must start with a letter between 'a' and 'h'")
+    
+    # extract y from square
+    y = int(square[1:2]) - 1
+
+    if (y >= 8 or y < 0):
+        raise ValueError("argument 'square' must end with a number between 1 and 8")
+
+    return {"x": x, "y": y}
