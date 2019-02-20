2bfae3001e86949db4a2d2a9a6e506d6daa0d362
felixxwu <felixxwu@gmail.com>
2019-02-20 03:13:26 +0000

    added assertions to util.py

--- util/util.py
+++ util/util.py
@@ -3,29 +3,21 @@ import json
 # maps a square string to [x, y] coordinates
 def squareToCoordinates(square):
 
-    if (len(square) != 2):
-        raise ValueError("argument 'square' must be two characters long")
+    assert len(square) == 2
 
     # extract letter from square
     letter = square[0]
-
-    if (not letter.isupper() and not letter.islower()):
-        raise ValueError("argument 'square' must start with a letter")
+    assert letter.isupper() or letter.islower()
 
     # force lowercase
     if (letter.isupper()): letter = chr(ord(letter) + 32)
 
     # convert letter to number
     x = ord(letter) - 97
-
-    if (x >= 8 or x < 0):
-        raise ValueError("argument 'square' must start with a letter between 'a' and 'h'")
+    assert x >= 0 and x < 8
     
     # extract y from square
     y = int(square[1]) - 1
-
-    if (y >= 8 or y < 0):
-        raise ValueError("argument 'square' must end with a number between 1 and 8")
+    assert y >= 0 and y < 8
 
     return {"x": x, "y": y}
-
