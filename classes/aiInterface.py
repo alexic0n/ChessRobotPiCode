f35fe99bcd36af65824be42b9bc328695c3d828d
Nikoleta Kuneva <43704382+nikoletakuneva@users.noreply.github.com>
2019-03-15 19:02:25 +0000

    Added speaker

--- classes/aiInterface.py
+++ classes/aiInterface.py
@@ -11,7 +11,7 @@ class ChessMatch:
     def __init__(self,thinkTime):
         self.limit = chess.engine.Limit(time = thinkTime)
         self.board = chess.Board()
-        self.engine = chess.engine.SimpleEngine.popen_uci("../util/stockfish-dice")
+        self.engine = chess.engine.SimpleEngine.popen_uci("/home/student/MachineLearning/pi/machinelearning/StockfishArm7/stockfish")
 
     def endgame(self):
         self.engine.quit()
