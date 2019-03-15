4299f559dd2dffc4379ec09c95877caa9521444c
Nikoleta Kuneva <43704382+nikoletakuneva@users.noreply.github.com>
2019-03-15 18:53:10 +0000

    Added speaker

--- machinelearning/main.py
+++ machinelearning/main.py
@@ -6,6 +6,7 @@ sys.path.append("../util/pythonchess")
 import chess
 import chess.engine
 import cv2 as cv
+import pyttsx3 as tts
 import glob
 sys.path.append("../")
 from classes.aiInterface import *
@@ -14,22 +15,36 @@ from util.util import *
 import requests
 import json
 
-def userTurn(board, computerSide, topleft, bottomright, WorB): #this basically just handles user interaction, reading the boardstate and update the internal board accordingly
+def userTurn(board, computerSide, topleft, bottomright, WorB, TextToSpeechEngine, vc): #this basically just handles user interaction, reading the boardstate and update the internal board accordingly
     if(board.legal_moves.count() == 0):
         print("Checkmate: Game Over!")
+        TextToSpeechEngine.say("Checkmate: Game Over!")
+        TextToSpeechEngine.runAndWait()        
         return False
     if(board.is_check()):
         print("You are in check...save your king!")
+        TextToSpeechEngine.say("You are in check...save your king!")
+        TextToSpeechEngine.runAndWait()
+
     continueGame = input("Type q to quit.")
     if(continueGame == "q"):
         return False
 
-    # print("\nMake your move on the board.") #if it returns false, the game ends, otherwise the game continues through another recursive loop
+    TextToSpeechEngine.say("Make your move on the board.")
+    TextToSpeechEngine.runAndWait()
     x = input("\nMake your move on the board.")
+
+    # Capture image
+    counter = 0
+    while(counter < 5):
+        ret,img = vc.read()
+        counter += 1
+    if img is None:
+        return "imagereadfail"  
+    cv.imwrite("images/image.jpg",img)    
+
     # Take last image from the webcam
-    paths = glob.glob('images/*.jpg')
-    img_path = paths[len(paths) - 1]
-    print(img_path)
+    img_path = "images/image.jpg"
     image = cv.imread(img_path)
 
     image = image[topleft[1]:bottomright[1], topleft[0]:bottomright[0]]
@@ -39,7 +54,6 @@ def userTurn(board, computerSide, topleft, bottomright, WorB): #this basically j
     probability_rank = '0'
     currentLegalMoves = getLegalMoves(board)
     while (True):
-        print(currentLegalMoves)
         print('Making request to Tardis.')
         r = requests.post("http://www.checkmate.tardis.ed.ac.uk/pieces", files={
             'board': open(img_path, 'rb'),
@@ -50,22 +64,31 @@ def userTurn(board, computerSide, topleft, bottomright, WorB): #this basically j
             'WorB': WorB
         })
 
-        print(r)
-
         data = r.json()
 
         userResponse = data['userResponse']
         if userResponse == 'true':
+            TextToSpeechEngine.say('Are you sure that you made a legal move?')
+            TextToSpeechEngine.runAndWait()
             is_legal = input('Are you sure that you made a legal move? y or n\n')
             if is_legal == 'y':
                     probability_rank = str(int(data['probability_rank']) + 1)
-
             else:
+                TextToSpeechEngine.say("Please make a new move.")
+                TextToSpeechEngine.runAndWait()
                 x = input("\nPlease make a new move.")
+
+                # Capture image
+                counter = 0
+                while(counter < 5):
+                    ret,img = vc.read()
+                    counter += 1
+                if img is None:
+                    return "imagereadfail"
+                cv.imwrite("images/image.jpg",img)
+
                 # Take last image from the webcam
-                paths = glob.glob('images/*.jpg')
-                img_path = paths[len(paths) - 1]
-                print(img_path)
+                img_path = "images/image.jpg"
                 image = cv.imread(img_path)
 
                 image = image[topleft[1]:bottomright[1], topleft[0]:bottomright[0]]
@@ -75,7 +98,9 @@ def userTurn(board, computerSide, topleft, bottomright, WorB): #this basically j
                 currentLegalMoves = getLegalMoves(board)
         else:
             data = r.json()
-            is_correct = input(data['status'] + " Is this correct? y or n \n")
+            TextToSpeechEngine.say(data['status'] + ". Is this correct?")
+            TextToSpeechEngine.runAndWait()
+            is_correct = input(data['status'] + ". Is this correct? y or n \n")
             if is_correct == 'y':
                 move_str = data['move']
                 move = chess.Move.from_uci(move_str)
@@ -84,14 +109,35 @@ def userTurn(board, computerSide, topleft, bottomright, WorB): #this basically j
                 return True
             else:
                 # NEXT PROBABLE MOVE
-                currentLegalMoves = [move for move in currentLegalMoves if not move == data['move']]
+                status_list = data['status'].split(" ")
+                piece = status_list[0]
+                origin = status_list[3]
+                TextToSpeechEngine.say("Have you moved a {} from {}?".format(piece, origin))
+                TextToSpeechEngine.runAndWait()
+                is_origin = input("Have you moved a {} from {}? y or n \n".format(piece, origin))
+         
+                if (is_origin == 'y'):
+                    currentLegalMoves = [move for move in currentLegalMoves if not move == data['move']]
+                else:
+                    probability_rank = str(int(data['probability_rank']) + 1)
 
         if len(currentLegalMoves) == 0:
+            TextToSpeechEngine.say("Your move is invalid. Please make a new move.")
+            TextToSpeechEngine.runAndWait()
             x = input("\nYour move is invalid. Please make a new move.")
+
+            # Capture image
+            counter = 0
+            while(counter < 5):
+                ret,img = vc.read()
+                counter += 1
+            if img is None:
+                return "imagereadfail"
+            cv.imwrite("images/image.jpg",img)
+
             # Take last image from the webcam
-            paths = glob.glob('images/*.jpg')
-            img_path = paths[len(paths) - 1]
-            print(img_path)
+            #paths = glob.glob('images/*.jpg')
+            img_path = "images/image.jpg"
             image = cv.imread(img_path)
 
             image = image[topleft[1]:bottomright[1], topleft[0]:bottomright[0]]
@@ -108,13 +154,32 @@ def getLegalMoves(board):
     return legalMoves
 
 def gameplayloop(board):
-    thinkTime = input("How long should I think per turn: ") #user can play as white or black, however for now it only works if white is at bottom of image and black is at top.
+    vc = cv.VideoCapture(0)
+
+    TextToSpeechEngine = tts.init()
+    TextToSpeechEngine.setProperty("volume", 15)
+    TextToSpeechEngine.setProperty("rate", 170)
+
     x = input("Please confirm the board is clear before proceeding.")
+    TextToSpeechEngine.say("Please confirm the board is clear before proceeding.")
+    TextToSpeechEngine.runAndWait()
+    thinkTime = input("How long should I think per turn: ") #user can play as white or black, however for now it only works if white is at bottom of image and black is at top.
+    TextToSpeechEngine.say("White or black?")
+    TextToSpeechEngine.runAndWait()
     worB = input("W or b?\n")
 
+    # Capture image
+    counter = 0
+    while(counter < 5):
+        ret,img = vc.read()
+        counter += 1
+    if img is None:
+        return "imagereadfail"
+    cv.imwrite("images/image.jpg",img)
+
     # Take last image from the webcam
-    paths = glob.glob('images/*.jpg')
-    img_path = paths[len(paths) - 1]
+    #paths = glob.glob('images/*.jpg')
+    img_path = "images/image.jpg"
     image = cv.imread(img_path)
 
     topleft, bottomright = segmentation_board(image) #find the coordinates of the board within the camera frame
@@ -127,14 +192,14 @@ def gameplayloop(board):
             board.push(x)
             print("AI makes move: {}.".format(x),"\n")
             print(board)
-            stopNow = userTurn(board, computerSide, topleft, bottomright, 'b')
+            stopNow = userTurn(board, computerSide, topleft, bottomright, 'b', TextToSpeechEngine, vc)
             print(board)
             if(not stopNow):
                 computerSide.endgame()
                 break
     else:
         while (True):
-            stopNow = userTurn(board, computerSide, topleft, bottomright, 'w')
+            stopNow = userTurn(board, computerSide, topleft, bottomright, 'w', TextToSpeechEngine, vc)
             print(board)
             if (not stopNow):
                 computerSide.endgame()
