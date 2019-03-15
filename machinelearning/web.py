ebf8e01df467facb148bba9a7b7558d34709d3c6
Nikoleta Kuneva <43704382+nikoletakuneva@users.noreply.github.com>
2019-03-15 18:59:58 +0000

    Added speaker

--- machinelearning/web.py
+++ machinelearning/web.py
@@ -36,52 +36,60 @@ def pieces():
 
     image_path = os.path.join(storage_path, name)
 
-    if not (request.files['board'] and request.files['fen'] and request.files['validmoves'] and request.files['userResp$        abort('401')
+    if not (request.files['board'] and request.files['fen'] and request.files['validmoves'] and request.files['userResponse'] and request.files['probability_rank'] and request.files['WorB']):
+        abort('401')
     request.files['board'].save(image_path)
 
     WorB = request.files['WorB'].read().decode()
     userResponse = request.files['userResponse'].read().decode()
     probability_rank = request.files['probability_rank'].read().decode()
-                                                                                                              
+    
     # Controls all other functions
     # Take last image from the webcam
     image = cv2.imread(image_path)
 
     # Crop the board into 64 squares
     crop_squares(image_path)
-
     # Move detection
     ## Detect the most probable origin square
-    (empty_square, piece) = detect_empty(model, request.files['fen'].read().decode(), userResponse, int(probability_ran$
-
+    (empty_square, piece) = detect_empty(model, request.files['fen'].read().decode(), userResponse, int(probability_rank), WorB)
+    
     ## Detect the most probable destination square
     moves = request.files['validmoves'].read().decode()
-
+    
     moves = moves.replace('[', '')
     moves = moves.replace(']', '')
     moves = moves.replace('\'', '')
     valid_moves = moves.split(', ')
     valid_moves = [move[2:4] for move in valid_moves if move[0:2] == empty_square]
     print(valid_moves)
-
+    
     if valid_moves == []:
         userResponse = 'true'
         r = json.dumps({'move':'-1', 'status':'-1', 'userResponse':userResponse, 'probability_rank':probability_rank})
         return r
 
     (piece_position) = detect_move(model, piece, valid_moves, WorB)
-
+    
     move = empty_square + piece_position
     print(move)
+    piece_dict = {
+        "p":"Pawn",
+        "b":"Bishop",
+        "n":"Knight",
+        "q":"Queen",
+        "r":"Rook",
+        "k":"King"
+    }
 
-    response = '{} moved from {} to {}'.format(piece, empty_square, piece_position)
-
+    response = '{} moved from {} to {}'.format(piece_dict.get(piece.lower()), empty_square, piece_position)
+    
     print(response)
-
+ 
     r = json.dumps({'move':move, 'status':response, 'userResponse':'false', 'probability_rank':probability_rank})
-
+    
     return r
-
+ 
 if __name__ == '__main__':
     #bjoern.run(app, '0.0.0.0', 8000)
     app.run(host='0.0.0.0', port=8000)
