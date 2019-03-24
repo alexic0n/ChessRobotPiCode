d9a7835046682905fdd8da1a1e3a94a25203c9aa
Alex Bruce <s1536094@vulcan.inf.ed.ac.uk>
2019-03-24 14:03:16 +0000

    Added untracked files : leos segmentation and crop in it's new location

---
+++ crop.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import cv2 as cv
+import os
+from PIL import Image
+app_root = os.path.dirname(os.path.abspath(__file__))
+
+def crop_squares(path):
+    # Crops a given image of a chessboard into 64 squares
+    src = cv.imread(path)
+    img = Image.open(path)
+    pixels = 224
+    prW = 0
+    prH = 0
+    i = 0
+    height, width, x = src.shape
+    sqH = height / 8
+    sqW = width / 8
+    k=8
+    while (i < 8):
+        letter = 'a'
+        start = i * 8
+        end = (i + 1) * 8
+
+        for j in range(start, end):
+            box = (prW, prH, sqW, sqH)
+            board = img.crop(box)
+            board = board.resize((pixels, pixels), Image.ANTIALIAS)
+            # Save the square into the Cropped folder
+            board.save(os.path.join(app_root, 'images', 'Cropped', ('{}{}.jpg').format(letter, k)))
+            prW = sqW
+            sqW = sqW + width / 8
+            letter = chr(ord(letter) + 1)
+        prW = 0
+        prH = height / 8 * (i + 1)
+        sqW = width / 8
+        sqH = height / 8 * (i + 2)
+        i = i + 1
+        k = k - 1
