a2afcdcb9484042deac6779caf03c0cc919a1180
felixxwu <felixxwu@gmail.com>
2019-03-31 22:14:23 +0100

    uncommented planner stuff

--- replay.py
+++ replay.py
@@ -1,8 +1,8 @@
 import sys
-# from util.planner import *
+from util.planner import *
 from util.util import *
-# from dictionary import print_play, play_sound
-# from mainUserMoves import text_to_speech, waitForConfirmationInput
+from dictionary import print_play, play_sound
+from mainUserMoves import text_to_speech, waitForConfirmationInput
 from classes.boardState import *
 from classes.move import *
 
@@ -15,17 +15,17 @@ def main(lang):
         moves = [current_place.rstrip() for current_place in filehandle.readlines()]
     
     if (len(moves) == 0):
-        # print_play("You haven't played any games yet.", lang)
+        print_play("You haven't played any games yet.", lang)
         sys.exit()
     
     worB = " "
     assert("user" in moves[0])
     if (moves[0][5] == 'b'):
         worB = 'b'
-        # print_play("You played with black.", lang)
+        print_play("You played with black.", lang)
     else:
         worB = 'w'
-        # print_play("You played with white.", lang)
+        print_play("You played with white.", lang)
     print(worB)
     for move in moves[1:]:
         moveDictFrom = squareToCoordinates(move[0:2], "w")
@@ -34,12 +34,12 @@ def main(lang):
         boardState.applyMove(moveObj)
         fen = "/".join(boardState.state)
         print(move, fen)
-        # print(len(move))
-        # plan(move, lang, worB)
+        print(len(move))
+        plan(move, lang, worB, board=fen)
         
         if(len(move) == 5):
-            # text_to_speech("Promotion! Please place the queen on {} and press yes when you are ready.".format(move[2:4]), lang)
-            # waitForConfirmationInput()
+            text_to_speech("Promotion! Please place the queen on {} and press yes when you are ready.".format(move[2:4]), lang)
+            waitForConfirmationInput()
             pass
                 
 if __name__ == '__main__':
