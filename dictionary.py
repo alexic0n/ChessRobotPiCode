7c723438063c4c6d20dd8022a955ae46e47294ef
Nikoleta Kuneva <43704382+nikoletakuneva@users.noreply.github.com>
2019-04-01 18:03:06 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -70,7 +70,7 @@ def print_play(text, lang):
         "Please start the calibration process. Refer to the instruction manual for help.":'calibration_start',
         "Calibration completed successfully.":'calibration_end',
         "Please set up the board, placing the black pieces on your side. Confirm by pressing yes.":'set_up_black',
-        "Please set up the board, placing the black pieces on your side. Confirm by pressing yes.":'set_up_white',
+        "Please set up the board, placing the white pieces on your side. Confirm by pressing yes.":'set_up_white',
         "Kasparov played with black.":'kasparov',
         "EV3 is not connected.":'no_ev3'
     }
