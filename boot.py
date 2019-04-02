e0ad56e2fddb107b6d90d7a30d6fb45e819f27ac
rphi <8237136+rphi@users.noreply.github.com>
2019-04-02 19:33:48 +0100

    Fix multithreading bootloader/game termination

--- boot.py
+++ boot.py
@@ -1,49 +1,33 @@
 import threading
 import subprocess
+import multiprocessing
 import keyboardleds
 from time import sleep
+from start import main
 
 on = False
 game = None
-numlock = keyboardleds.LedKit(
-    '/dev/input/by-id/usb-04d9_1203-event-kbd').num_lock
 
 
 def gameStopped():
     print("Game process stopped")
     game = None
     on = False
-    numlock.reset()
+    # clear numlock
+    subprocess.call(['/usr/bin/setleds', '-num'])
 
-
-def popenAndCall(onExit, *popenArgs, **popenKWArgs):
-    """
-    Runs a subprocess.Popen, and then calls the function onExit when the
-    subprocess completes.
-
-    Use it exactly the way you'd normally use subprocess.Popen, except include a
-    callable to execute as the first argument. onExit is a callable object, and
-    *popenArgs and **popenKWArgs are simply passed up to subprocess.Popen.
-    """
-    def runInThread(onExit, popenArgs, popenKWArgs):
-        proc = subprocess.Popen(*popenArgs, **popenKWArgs)
-        proc.wait()
-        onExit()
-        return
-
-    thread = threading.Thread(target=runInThread,
-                              args=(onExit, popenArgs, popenKWArgs))
-    thread.start()
-
-    return thread  # returns immediately after the thread starts
+def startandwait():
+    main()
+    gameStopped()
 
 
 if __name__ == "__main__":
     print("CheckMate Bootstrapper starting.")
-    numlock.reset()
+    # clear numlock
+    subprocess.call(['/usr/bin/setleds', '-num'])
     while True:
-        state = numlock.get()
-        if state == True:
+        state = subprocess.check_output('/usr/bin/setleds')
+        if b'on' in state:
             # num lock on
             if not on:
                 # requested game start
@@ -51,7 +35,9 @@ if __name__ == "__main__":
                 if game != None:
                     # something's gone wrong
                     raise Exception("Game still exists, cannot turn on.")
-                game = popenAndCall(gameStopped, 'python3 ./start.py')
+                #game = popenAndCall(gameStopped, ['python3', './start.py'])
+                game = multiprocessing.Process(target=startandwait)
+                game.start()
                 on = True
 
         else:
@@ -59,8 +45,7 @@ if __name__ == "__main__":
             if on:
                 # requested game terminate
                 print("Killing game")
-                game.kill()
+                game.terminate()
                 on = False
                 game = None
-    sleep(0.5)  
-
+    sleep(0.5)
